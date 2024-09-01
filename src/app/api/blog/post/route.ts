99df1bfd6815a89df08872d8de2155868163a553
nadeem <nadeemsangrasi903@gmail.com>
2024-09-01 14:21:57 +0500

    add update post route

--- src/app/api/blog/post/route.ts
+++ src/app/api/blog/post/route.ts
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../../auth/[...nextauth]/options";
 import { db, postsTable } from "@/lib/database";
+import { and, eq } from "drizzle-orm";
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
   const session = await getServerSession(req, res, authOptions);
@@ -35,10 +36,13 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
       })
       .returning();
 
-    return NextResponse.json({
-      success: true,
-      message: "post published successfully",
-    });
+    return NextResponse.json(
+      {
+        success: true,
+        message: "post published successfully",
+      },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("something went wrong", error);
     return NextResponse.json(
@@ -48,16 +52,89 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
   }
 };
 
-export const GET = async (req: NextRequest) => {
+export const GET = async () => {
   try {
     const posts = await db.select().from(postsTable);
 
-    return NextResponse.json({ success: true, data: posts });
+    return NextResponse.json({ success: true, data: posts }, { status: 200 });
   } catch (error) {
     console.error("Failed to fetch posts", error);
 
     return NextResponse.json(
       { success: false, message: "Failed to fetch posts" },
+      { status: 500 }
+    );
+  }
+};
+
+export const PATCH = async (req: NextRequest, res: NextResponse) => {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!session || !session.user) {
+    console.error("User not authenticated");
+    return NextResponse.json(
+      { success: false, message: "User not authenticated" },
+      { status: 404 }
+    );
+  }
+
+  try {
+    const { postId, userId, categoryId, title, content, imageUrl, slug } =
+      await req.json();
+
+    if (
+      !postId ||
+      !userId ||
+      !categoryId ||
+      !title ||
+      !content ||
+      !imageUrl ||
+      !slug
+    ) {
+      console.error("All fields are required");
+      return NextResponse.json(
+        { success: false, message: "All fields are required" },
+        { status: 500 }
+      );
+    }
+
+    if (session.user.id !== userId) {
+      console.error("You are not authorised to update this post");
+      return NextResponse.json(
+        {
+          success: false,
+          message: "You are not authorised to update this post",
+        },
+        { status: 400 }
+      );
+    }
+
+    const updatedPost = await db
+      .update(postsTable)
+      .set({
+        categoryId,
+        title,
+        imageUrl,
+        content,
+        slug,
+        createdAt: new Date(),
+      })
+      .where(and(eq(postsTable.id, postId), eq(postsTable.userId, userId)))
+      .returning();
+
+    if (updatedPost.length === 0) {
+      console.error("Post not found");
+      return NextResponse.json({ success: false, message: "Post not found" });
+    }
+
+    return NextResponse.json(
+      { success: true, message: "Post updated successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Failed to update post", error);
+    return NextResponse.json(
+      { success: false, message: "Failed to update post" },
       { status: 500 }
     );
   }
