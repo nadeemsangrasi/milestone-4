3d72931e50bca64504c02498891fb55a0ae38a7d
nadeem <nadeemsangrasi903@gmail.com>
2024-09-01 16:38:00 +0500

    add category route and completed backend

--- src/app/api/blog/category/route.ts
+++ src/app/api/blog/category/route.ts
@@ -1,2 +1,29 @@
+import { categoriesTable, db } from "@/lib/database";
+import { NextRequest, NextResponse } from "next/server";
 
+export const GET = async (req: NextRequest) => {
+  try {
+    const categories = await db.select().from(categoriesTable);
+    if (categories.length === 0) {
+      console.error("Error  gettng categories");
+      return NextResponse.json(
+        { success: false, message: "Error getting categories" },
+        { status: 500 }
+      );
+    }
 
+    return NextResponse.json(
+      { success: true, data: categories },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error getting categories", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Error getting categories",
+      },
+      { status: 500 }
+    );
+  }
+};
