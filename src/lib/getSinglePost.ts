558c719297b1e27395b042563833a2989f7c6d4f
Nadeem Khan <140242307+nadeemsangrasi@users.noreply.github.com>
2024-09-14 11:19:09 +0500

    Update getSinglePost.ts

--- src/lib/getSinglePost.ts
+++ src/lib/getSinglePost.ts
@@ -1,14 +1,12 @@
 export const getSinglePost = async (slug: string) => {
   try {
-    const res = await fetch(`https://milestonee-4.vercel.app/api/blog/post/single-post/${slug}`, {
-      method: 'GET',
-      headers: {
-        'Cache-Control': 'no-store',  
-        'Pragma': 'no-cache',        
-        'Expires': '0',               
-      },
-      cache: 'no-store', 
-    });
+    const res = await fetch(
+      `https://milestonee-4.vercel.app/api/blog/post/single-post/${slug}`,
+      {
+        method: "GET",
+        cache: "force-cache",
+      }
+    );
 
     if (!res.ok) {
       throw new Error(`Failed to fetch the post data: ${res.statusText}`);
@@ -17,7 +15,7 @@ export const getSinglePost = async (slug: string) => {
     const data = await res.json();
     return data.data;
   } catch (error) {
-    console.error('Error fetching single post:', error);
+    console.error("Error fetching single post:", error);
     return null;
   }
 };
