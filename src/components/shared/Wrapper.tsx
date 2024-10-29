c612d202262a0104c89235d527343ed6a975ffb2
Nadeem Khan <140242307+nadeemsangrasi@users.noreply.github.com>
2024-10-29 23:40:13 +0500

    Update Wrapper.tsx

--- src/components/shared/Wrapper.tsx
+++ src/components/shared/Wrapper.tsx
@@ -1,6 +1,6 @@
 const Wrapper = ({ children }: { children: React.ReactNode }): JSX.Element => {
   return (
-    <div className="w-full px-4 sm:px-0 sm:w-3/4  mx-auto  py-8 ">
+    <div className="w-full px-4 sm:px-0 sm:max-w-screen-xl mx-auto  py-8 ">
       {children}
     </div>
   );
